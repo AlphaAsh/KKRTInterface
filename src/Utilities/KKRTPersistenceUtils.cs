ba93a1d112c0d43b75e3bb257019bf485159ebe6
Ash <ashleygrahamhall@hotmail.com>
2016-05-02 14:37:55 +0100

    Provide a constructed Guid, rather than map one
    May have to tinker with the exact format of the constructed string but
    regardless, this means no need to store mapped Guids - they are
    constructed from a key all groundstations already have.

--- src/Utilities/KKRTPersistenceUtils.cs
+++ src/Utilities/KKRTPersistenceUtils.cs
@@ -81,6 +81,7 @@ namespace KerKonRTInterface.Utilities
 					obj.setSetting("RTGuid", NewGuid);
 					Debug.Log("KK: Added RT GroundStation " + NewGuid.ToString());
 					KerbalKonstructs.Utilities.PersistenceUtils.saveStaticPersistence(obj);
+					RemoteTech.API.API.DirectConfigureStationAntenna(NewGuid, (float)obj.getSetting("TrackingShort"), 0, 1);
 					Debug.Log("KK: Saved station object");
 				}
 				else
@@ -88,6 +89,19 @@ namespace KerKonRTInterface.Utilities
 					Debug.Log("KK: The station just opened already had a guid???");
 				}
 			}
+		}
+
+		public static string ConstructGuidString(StaticObject obj)
+		{
+			string sGuid = "";
+
+			Vector3 vPos = (Vector3)obj.getSetting("RadialPosition");
+			string sPos = vPos.ToString();
+
+			string sHeight = obj.getSetting("RadiusOffset").ToString();
+
+			sGuid = sHeight.Replace(".", "") + sPos.Replace("@", "").Replace(",", "").Replace(".", "").Replace(";", "").Replace("'", "").Replace(" ", "");
+			return sGuid;
 		}
 
 		public static void OpenRTGroundStation(StaticObject obj)
@@ -102,6 +116,8 @@ namespace KerKonRTInterface.Utilities
 
 			if ((string)obj.getSetting("FacilityType") == "TrackingStation")
 			{
+				Vector3 vPos = (Vector3)obj.getSetting("RadialPosition");
+				string sPos = vPos.ToString();
 				CelestialBody CelBody = (CelestialBody)obj.getSetting("CelestialBody");
 				var objectpos = CelBody.transform.InverseTransformPoint(obj.gameObject.transform.position);
 				var dObjectLat = NavUtils.GetLatitude(objectpos);
@@ -117,50 +133,36 @@ namespace KerKonRTInterface.Utilities
 				float fHeight = (float)obj.getSetting("RadiusOffset");
 				double dHeight = (double)fHeight;
 				int iBody = 1;
-				Guid NewGuid;
-				Guid gGuid = (Guid)obj.getSetting("RTGuid");
 
-				if (gGuid == Guid.Empty)
-				{
-					NewGuid = RemoteTech.API.API.AddGroundStation(sTSName, disObjectLat, disObjectLon, dHeight, iBody);
+				string sGuid = ConstructGuidString(obj);
 
-					if (NewGuid == Guid.Empty)
-					{
-						Debug.Log("KK: RT handed back an empty guid!");
-					}
+				RemoteTech.API.API.AddGroundStation(sGuid, sTSName, disObjectLat, disObjectLon, dHeight, iBody);
 
-					obj.setSetting("RTGuid", NewGuid);
-					Debug.Log("KK: Added RT GroundStation " + NewGuid.ToString());
-					KerbalKonstructs.Utilities.PersistenceUtils.saveStaticPersistence(obj);
-					Debug.Log("KK: Saved station object");
-				}
+				Debug.Log("KK: Added RT GroundStation " + sGuid);
+				KerbalKonstructs.Utilities.PersistenceUtils.saveStaticPersistence(obj);
+				Debug.Log("KK: Saved station object");
 			}
 		}
 
-		public static Guid gEmpty = Guid.Empty;
-
-		public static void CloseRTGroundStation()
+		public static void CloseRTGroundStation(StaticObject obj)
 		{
 			bool bRTLoaded = AssemblyLoader.loadedAssemblies.Any(a => a.name == "RemoteTech");
 			if (!bRTLoaded) return;
-
-			StaticObject obj = KerbalKonstructs.UI.SharedInterfaces.getStoredEventObject();
 			if (obj == null) return;
 
 			if ((string)obj.getSetting("FacilityType") == "TrackingStation")
 			{
-				Guid gGuid = (Guid)obj.getSetting("RTGuid");
+				string sGuid = ConstructGuidString(obj);
 				
-				if (gGuid != Guid.Empty)
+				if (sGuid != "")
 				{
 					Debug.Log("KK: Attempting to remove RT GroundStation");
-					bool bRemove = RemoteTech.API.API.RemoveGroundStation(gGuid);
+					Guid nGuid = new Guid(sGuid);
+					bool bRemove = RemoteTech.API.API.RemoveGroundStation(nGuid);
 					if (bRemove)
 						Debug.Log("KK: Removed RT GroundStation");
 					else
-						Debug.Log("KK: Failed to remove RT GroundStation");
-
-					obj.setSetting("RTGuid", Guid.Empty);
+						Debug.Log("KK: Failed to remove RT GroundStation " + sGuid);
 
 					KerbalKonstructs.Utilities.PersistenceUtils.saveStaticPersistence(obj);
 					Debug.Log("KK: Saved station object");
@@ -172,7 +174,7 @@ namespace KerKonRTInterface.Utilities
 			}
 		}
 
-		public static void CloseRTGroundStation(StaticObject obj, Guid gGuid)
+		/* public static void CloseRTGroundStation(StaticObject obj, Guid gGuid)
 		{
 			bool bRTLoaded = AssemblyLoader.loadedAssemblies.Any(a => a.name == "RemoteTech");
 			if (!bRTLoaded) return;
@@ -182,10 +184,9 @@ namespace KerKonRTInterface.Utilities
 				if (gGuid != Guid.Empty)
 				{
 					RemoteTech.API.API.RemoveGroundStation(gGuid);
-					obj.setSetting("RTGuid", Guid.Empty);
 				}
 			}
-		}
+		} */
 
 		public static void UpdateRTGroundStations()
 		{
@@ -215,15 +216,13 @@ namespace KerKonRTInterface.Utilities
 					else
 					{
 						KerbalKonstructs.Utilities.PersistenceUtils.loadStaticPersistence(obj);
-
-						Guid gGuid2 = (Guid)obj.getSetting("RTGuid");
-						CloseRTGroundStation(obj, gGuid2);
+						CloseRTGroundStation(obj);
 					}
 				}
 			}
 		}
 
-		public static void saveRTCareerBackup()
+		/* public static void saveRTCareerBackup()
 		{
 			bool bRTLoaded = AssemblyLoader.loadedAssemblies.Any(a => a.name == "RemoteTech");
 			if (!bRTLoaded) return;
@@ -236,9 +235,9 @@ namespace KerKonRTInterface.Utilities
 			// Back it up
 			string backupConfigPath = string.Format("{0}saves/{1}/KKRTCareerBack.cfg", KSPUtil.ApplicationRootPath, HighLogic.SaveFolder);
 			File.Copy(rtConfigPath, backupConfigPath, true);
-		}
+		} */
 
-		public static void loadRTCareerBackup()
+		/* public static void loadRTCareerBackup()
 		{
 			bool bRTLoaded = AssemblyLoader.loadedAssemblies.Any(a => a.name == "RemoteTech");
 			if (!bRTLoaded) return;
@@ -252,7 +251,7 @@ namespace KerKonRTInterface.Utilities
 			string backupConfigPath = string.Format("{0}saves/{1}/KKRTCareerBack.cfg", KSPUtil.ApplicationRootPath, HighLogic.SaveFolder);
 			if (!File.Exists(backupConfigPath)) return;
 			File.Copy(backupConfigPath, rtConfigPath, true);
-		}
+		} */
 
 		/* public static void backupRemoteTechConfig()
 		{
